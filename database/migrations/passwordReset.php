ccf772ca87093565e2ddb95d1fe48c73aeef8c8b
D9Coy <davidcoy9@hotmail.com>
2016-11-21 00:19:48 -0500

    version1.0.1

---
+++ database/migrations/passwordReset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class passwordReset extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'password_resets';
+
+    protected $fillable = [
+        'email', 'token',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    
+
+}
