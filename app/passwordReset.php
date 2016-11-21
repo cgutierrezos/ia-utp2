c001af946c6304aa6e9ec7bd959881dfc442cabc
D9Coy <davidcoy9@hotmail.com>
2016-11-21 01:39:52 -0500

    version1.0.1

--- app/passwordReset.php
+++ app/passwordReset.php
@@ -14,6 +14,7 @@ class passwordReset extends Model
      */
     protected $table = 'password_resets';
 
+    protected $primaryKey = 'email';
 
     protected $fillable = [
         'email', 'token',
