523c2de1be8b4ddf8a17fa2584a2889a414d7d1c
D9Coy <davidcoy9@hotmail.com>
2016-11-20 23:45:20 -0500

    version1.0.1

--- resources/views/templates/banner_animation.blade.php
+++ resources/views/templates/banner_animation.blade.php
@@ -6,12 +6,12 @@
 		
 	<ul class="actions" style="text-align: center">
 		<li>
-			<a id="banimar" class="button big" onclick='inicio_animacion()' style="width: 400px">
+			<a id="banimar" class="button big" onclick='inicio_animacion()'>
 				<span id="play_animar" class="glyphicon glyphicon-play" aria-hidden="true"></span> Animar (Automatico)
 			</a>
 		</li>
 		<li>
-			<a id="siguiente" class="button big" onclick='animar()' style="width: 400px">
+			<a id="siguiente" class="button big" onclick='animar()'>
 				<span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span> Animar (Manual)
 			</a>
 		</li>
@@ -21,13 +21,13 @@
 
 	<ul class="actions" style="text-align: center">
 		<li>
-			<a class="button special big" onclick='javascript:location.reload()' style="width: 400px">
+			<a class="button special big" onclick='javascript:location.reload()'>
 				<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Recargar Pagina
 			</a>
 		</li>
 		@if($search == 2)
 			<li>
-				<a class="button special big" href="/animaciones/grafo/get-ruta/<?php echo $grafo ?>" class="button small" style="width: 400px"><span>Animar Otra Ruta</span>
+				<a class="button special big" href="/animaciones/grafo/get-ruta/<?php echo $grafo ?>" class="button small"><span>Animar Otra Ruta</span>
 				</a>
 			</li>
 		@endif
