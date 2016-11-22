176ed7a1de0159cf72f1316d09d1d63e78be1ba3
D9Coy <davidcoy9@hotmail.com>
2016-11-22 00:05:55 -0500

    version1.0.1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,8 +14,6 @@
                             </span>
                         @endif
                     </div>
-                </div>
-                <div class="row uniform 50%">
                     <div class="12u$">
                         <input type="password" name="password" id="password" value="" placeholder="Password">
                         @if ($errors->has('password'))
@@ -24,13 +22,9 @@
                             </span>
                         @endif
                     </div>
-                </div>
-                <div class="row uniform 50%">
                     <div class="12u$">
                         <a href="/password/forgot">Olvido su clave?</a>
                     </div>
-                </div>
-                <div class="row uniform 50%">
                     <div class="12u$">
                         <ul class="actions">
                             <li><input type="submit" value="Guardar" class="special"></li>
